f35673419128bca9103490f8b4a2164517bca6cb
salymilda11-prog <salymilda11@gmail.com>
2026-08-18 15:02:26 +0530

    commit4

--- tests/modifiers.spec.js
+++ tests/modifiers.spec.js
@@ -1,6 +1,6 @@
-///Modifiers or Tags
-test.only() //to run a particlr test
-test.skip() //to skip a test
-test.fixme() //fix it later this test is broken
-test.fail() //the test expctd to fail
-test.slow() //test is expected to take longer
+// ///Modifiers or Tags
+// test.only() //to run a particlr test
+// test.skip() //to skip a test
+// test.fixme() //fix it later this test is broken
+// test.fail() //the test expctd to fail
+// test.slow() //test is expected to take longer
